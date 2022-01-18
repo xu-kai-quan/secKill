9f417b79e7b82c940d813147e2819221ddb8cd55
xu-kai-quan <964262001@qq.com>
2022-01-18 22:10:10 +0800

    staticize order detail and solve the oversold and Order cache optimization

--- target/classes/db/migration/V2__Insert.sql
+++ target/classes/db/migration/V2__Insert.sql
@@ -5,7 +5,7 @@ INSERT INTO GOODS
 VALUES (1, 'iphoneX', 'Apple iPhone X (A1865) 64GB 银色 移动联通电信4G手机', '/img/iphonex.png',
         'Apple iPhone X (A1865) 64GB 银色 移动联通电信4G手机', 8765.00, 10000),
        (2, '华为Meta9', '华为 Mate 9 4GB+32GB版 月光银 移动联通电信4G手机 双卡双待', '/img/meta10.png',
-        '华为 Mate 9 4GB+32GB版 月光银 移动联通电信4G手机 双卡双待', 3212.00, -1),
+        '华为 Mate 9 4GB+32GB版 月光银 移动联通电信4G手机 双卡双待', 3212.00, 10000),
        (3, 'iphone8', 'Apple iPhone 8 (A1865) 64GB 银色 移动联通电信4G手机', '/img/iphone8.png',
         'Apple iPhone 8 (A1865) 64GB 银色 移动联通电信4G手机', 5589.00, 10000),
        (4, '小米6', '小米6 4GB+32GB版 月光银 移动联通电信4G手机 双卡双待', '/img/mi6.png',
